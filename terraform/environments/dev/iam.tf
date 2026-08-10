04c728fc990886519b1ebd521b8424500ff380c4
juliazam <juliz@mail.ru>
2026-08-10 20:14:50 +0800

    feat(iam): add glue-etl-role with trust policy and attach read policy

--- terraform/environments/dev/iam.tf
+++ terraform/environments/dev/iam.tf
@@ -19,3 +19,22 @@ resource "aws_iam_policy" "glue_raw_read" {
         ]
     })
 }
+
+resource "aws_iam_role" "glue_etl_role" {
+    name = "glue-etl-role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Effect    = "Allow"
+            Principal = { Service = "glue.amazonaws.com" }
+            Action    = "sts:AssumeRole"
+        }
+        ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_etl_read" {
+    role = aws_iam_role.glue_etl_role.name
+    policy_arn = aws_iam_policy.glue_raw_read.arn
+}
