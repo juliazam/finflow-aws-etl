cc25176bccd2560ca9e540a0e70e514203a815f8
juliazam <juliz@mail.ru>
2026-08-11 16:21:21 +0800

    feat(glue): define raw_transactions table explicitly (Ministack crawler doesn't classify schema)

--- terraform/environments/dev/glue.tf
+++ terraform/environments/dev/glue.tf
@@ -11,3 +11,23 @@ resource "aws_glue_crawler" "finflow_db_crawler" {
         path = "s3://${var.raw_bucket_name}/"
     }
 }
+
+resource "aws_glue_catalog_table" "raw_transactions" {
+  name          = "raw_transactions"
+  database_name = aws_glue_catalog_database.finflow_db.name
+
+  storage_descriptor {
+    location      = "s3://${var.raw_bucket_name}/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    columns {
+      name = "id"
+      type = "string"
+    }
+    columns {
+      name = "amount"
+      type = "double"
+    }
+  }
+}
